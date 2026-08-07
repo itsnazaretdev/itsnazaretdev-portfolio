4f668dece1d64d39f9bed85d5838b776ad1282af
Nazaret <175538231+itsnazaretdev@users.noreply.github.com>
2026-08-07 22:32:11 +0200

    add contact and about me

---
+++ src/data/nav.js
@@ -0,0 +1,13 @@
+export const navLinks = [
+  { label: "Proyectos", href: "#projects" },
+  { label: "Skills", href: "#skills" },
+  { label: "Sobre mí", href: "#about" },
+  { label: "Contacto", href: "#contact" },
+
+];
+
+export const socialLinks = [
+  { label: "github", href: "https://github.com/tu-usuario" },
+  { label: "linkedin", href: "https://linkedin.com/in/tu-usuario" },
+  { label: "email", href: "mailto:nazaret@email.dev" },
+];
